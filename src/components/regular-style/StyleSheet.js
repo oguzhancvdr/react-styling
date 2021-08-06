526394a2c244c82387d8d8e4df31e133e2fa4318
ogzhn <oguzhancavdar06@gmail.com>
2021-08-06 16:22:46 +0300

    rm className attribute

--- src/components/regular-style/StyleSheet.js
+++ src/components/regular-style/StyleSheet.js
@@ -4,11 +4,10 @@ const StyleSheet = () => {
   return (
     <div>
       <img
-        className="logo"
         src="https://secure.meetupstatic.com/photos/event/3/1/b/9/600_488352729.jpeg"
         alt="clarusway-logo"
       />
-      <h1 >Welcome to Clarusway</h1>
+      <h1>Welcome to Clarusway</h1>
       <p>Change your skillset, change your future!</p>
     </div>
   );
