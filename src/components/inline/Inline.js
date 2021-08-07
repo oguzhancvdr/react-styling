ab4db552a6dc9c54bc40017d2184ff367ddb149b
ogzhn <oguzhancavdar06@gmail.com>
2021-08-07 12:28:18 +0300

    complete inline styyling [CW-123]

--- src/components/inline/Inline.js
+++ src/components/inline/Inline.js
@@ -1,13 +1,15 @@
-const myStyles = {
-  par : {
-    color:"blue",
-    fontSize:"1.2rem",
-    fontWeight:"bolder"
-  },
-  secondDiv: {
-    backgroundColor:"#b0b0b0"
-  }
-}
+// const myStyles = {
+//   par : {
+//     color:"blue",
+//     fontSize:"1.2rem",
+//     fontWeight:"bolder"
+//   },
+//   secondDiv: {
+//     backgroundColor:"#b0b0b0"
+//   }
+// }
+
+import {myStyles} from './styles';
 
 const Inline = () => {
   return (
---
+++ src/components/inline/styles.js
@@ -0,0 +1,10 @@
+ export const myStyles = {
+    par : {
+      color:"blue",
+      fontSize:"1.2rem",
+      fontWeight:"bolder"
+    },
+    secondDiv: {
+      backgroundColor:"#b0b0b0"
+    }
+  }
