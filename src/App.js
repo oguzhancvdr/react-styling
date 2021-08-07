2af0ae75eedd3c356d374c223b213f12d959b987
ogzhn <oguzhancavdar06@gmail.com>
2021-08-07 12:54:37 +0300

    create sass style flies and comp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import Inline from "./components/inline/Inline"
-import StyleSheet from "./components/regular-style/StyleSheet"
+// import StyleSheet from "./components/regular-style/StyleSheet"
+import Sassy from "./components/sassy/Sassy"
 
 const App = () => {
   return (
     <div>
       {/* <Inline /> */}
-      <StyleSheet primary={false} />
+      {/* <StyleSheet primary={false} /> */}
+      <Sassy />
     </div>
   );
 };
--- src/components/sassy/Sassy.js
+++ src/components/sassy/Sassy.js
@@ -1,11 +1,17 @@
+import '../../sass/style.scss';
+
 const Sassy = () => {
   return (
-    <div>  
+    <div className="container">  
         <h1>Sassy in React</h1>
         <p>
           Sass stands for Syntactically Awesome Stylesheets which is a CSS
           extension that gives us more powerful CSS.
         </p>
+        <div className="button-container">
+          <button className="button button-container--small">Small</button>
+          <button className="button button-container--large">Large</button>
+        </div>
     </div>
   );
 };
